34afd7345dc59e13acbf1749918ac7c3b3804cd9
langat justus <justuslangat78@gmail.com>
2020-08-26 20:27:55 +0300

    created a function for the services

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -9,5 +9,14 @@ export class ProfileService {
   private clientid ='26daead3a990db906a05';
   private clientsecret='89bb2323352ed0be5f4e68269dc8e5b74c12b110';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+    console.log("its okay");
+    this.username='Langat05';
+   }
+
+   getdata(){
+     return this.http.get("https://github.com/users/" + this.username +"?client_id" + this.clientid
+     + "&client_secret=" + this.clientsecret)
+     
+   }
 }
