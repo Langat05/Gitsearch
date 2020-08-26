ba85e03520625c512e9f7d70b5694c2361e800e4
langat justus <justuslangat78@gmail.com>
2020-08-26 20:14:53 +0300

    created properties for the service

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http'
+import  'rxjs/Rx';
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private username:string;
+  private clientid ='';
+  private clientsecret='';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 }
