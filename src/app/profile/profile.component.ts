8ec86b5f239a92d389f0c8883c41a65303a5a8f6
langat justus <justuslangat78@gmail.com>
2020-08-28 11:31:39 +0300

    modified the navbar html file

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,38 +4,34 @@ import { ProfileService } from '../profile.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
   profile: any[];
   repos: any[];
-  username:string;
+  username: string;
 
   constructor(private profileservice: ProfileService) {
     this.profileservice.getdata().subscribe((profile: any[]) => {
       console.log(profile);
-      this.profile = profile
+      this.profile = profile;
     });
     this.profileservice.getProfileRepos().subscribe((repos: any[]) => {
       console.log(repos);
       this.repos = repos;
-
     });
   }
-  findProfile(){
+  findProfile() {
     this.profileservice.updateProfile(this.username);
     this.profileservice.getdata().subscribe((profile: any[]) => {
       console.log(profile);
-      this.profile = profile
+      this.profile = profile;
     });
     this.profileservice.getProfileRepos().subscribe((repos: any[]) => {
       console.log(repos);
       this.repos = repos;
-
     });
   }
-  
-  ngOnInit() {
-  }
-}
 
+  ngOnInit() {}
+}
