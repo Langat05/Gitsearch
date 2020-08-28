4acb62d4c8e97f657911197b5cb17ef01e72b48a
langat justus <justuslangat78@gmail.com>
2020-08-28 11:36:30 +0300

    implemented the router links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { RepositoryComponent } from './repository/repository.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', component:ProfileComponent},
+  {path:'users', component:ProfileComponent},
+  {path:'repositories', component:RepositoryComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
